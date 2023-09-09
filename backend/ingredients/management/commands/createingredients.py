bd6a08e12d67a9d5fde8df52d2dfba43f426cd8a
Alexander Nepein <alekswonder@yandex.ru>
2023-09-09 23:27:06 +0500

    Add managment command that creates ingredients

---
+++ backend/ingredients/management/commands/createingredients.py
@@ -0,0 +1,23 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from ingredients.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Create ingredients for app'
+
+    def handle(self, *args, **options):
+        file_path = '../data/ingredients.csv'
+        with open(file_path, 'r', encoding='utf-8') as ingredients_csv:
+            ingredients = [
+                kwargs for kwargs in
+                csv.DictReader(ingredients_csv,
+                               fieldnames=('name', 'measurement_unit'),
+                               delimiter=',')
+            ]
+            Ingredient.objects.bulk_create(
+                [Ingredient(**kwargs) for kwargs in ingredients]
+            )
+        self.stdout.write(self.style.SUCCESS('All ingredients created!'))
