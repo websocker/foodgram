14a8a158b2ef41c2eab5b17ba4a38babf6578c74
Alexander Nepein <alekswonder@yandex.ru>
2023-09-25 16:29:11 +0500

    Edit code according flake8

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,4 @@
 from django.db.models import Sum
-
 from recipes.models import Ingredient, RecipeIngredient
 
 
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-
 from users.models import Follow
 
 User = get_user_model()
